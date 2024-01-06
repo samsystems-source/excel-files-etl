825631e61a49a2654afaf4a3bdef54b76be0d774
tyagosampaio <tyagosampaio@outlook.com>
2024-01-05 22:52:33 -0300

    etl-teste-ciandcd-20240105

--- app/main.py
+++ app/main.py
@@ -7,3 +7,4 @@ if __name__ == "__main__":
     data_frame_list = extract_from_excel('data/input')
     data_frame = concat_data_frame(data_frame_list)
     load_excel(data_frame, "data/output", "final_file")
+    print("Arquivo carregado com sucesso!.")
